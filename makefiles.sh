1bfcd8296621b927a00ea40274ad24dbeac9b980
Jim Havrilla <jmh2tt@JimBook.local>
2017-11-02 21:32:25 -0400

    added chrom sizes file, fixed makefiles.sh

--- makefiles.sh
+++ makefiles.sh
@@ -6,4 +6,4 @@ fi
 zcat ~/analysis/essentials/gnomadbased-ccrs.bed.gz | sort -k14,14nr | awk 'BEGIN{key=""; val=0} {{if (key !=$4 $7) val+=1} print $0 "\t" val; key=$4 $7}' | cut -f -4,7-8,10- | sort -k1,1 -k2,2n | cat <(printf "chrom\tstart\tend\tgene\tranges\tvarflag\tcpg\tcov_score\tresid\tresid_pctile\tccr_pct\tunique_key\n") - | bgzip -c > ccrs.bed.gz
 zcat ccrs.bed.gz | sed '1d' | sed 's/^/chr/g' | awk '{print $1,$2,$3,$11}' OFS="\t" | bedtools merge -d -1 -c 4 -o mean > ccrs.bedGraph
 bgzip -c ccrs.bedGraph > ccrs.bedGraph.gz; tabix -b 2 -e 3 ccrs.bedGraph.gz
-bedGraphToBigWig ccrs.bedGraph $DATA/hg19.chrom.sizes ccrs.bw
+bedGraphToBigWig ccrs.bedGraph hg19.chrom.sizes ccrs.bw
